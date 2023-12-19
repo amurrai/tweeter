7b84835330759c9ea27acb2dc153858e1bb228b8
amurrai <andre.murrai@gmail.com>
2023-12-19 13:31:43 -0500

    implement client.js function to access and render tweet database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(data) {
+    const $tweet = `
+    <article class="tweet">
+          <header>
+            <div class="tweet-avatar">
+              <img src=${data.user.avatars}> 
+              <h3>${data.user.name}</h3>
+            </div>
+            <div>
+              <h3>${data.user.handle}</h3>
+            </div>
+          </header>
+          <p> ${data.content.text} </p>
+          <footer>
+            ${data.created_at}
+            <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+    `;
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  }
+
+  renderTweets(data);
+});
