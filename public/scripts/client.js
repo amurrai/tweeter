92bcce4eae3db9fc23bc38e1250b0c54696b0467
amurrai <andre.murrai@gmail.com>
2023-12-19 15:49:34 -0500

    implement loadTweets function to access the tweet database and render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const createTweetElement = function(data) {
     const $tweet = `
     <article class="tweet">
@@ -60,9 +35,16 @@ $(document).ready(() => {
     for (let tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
     }
-  }
+  };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    .then(function (data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 
   const $button = $('#create-new-tweet');
   $button.on('submit', function () {
